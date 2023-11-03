307454b79104a04053133c6bd855cdb059a0eac9
Peter Boakye <petboa123@gmail.com>
2023-11-03 19:46:38 +0000

    Update 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -33,7 +33,7 @@ def get_locale() -> str:
 @app.route("/")
 def Hello() -> str:
     """ Home page"""
-    return render_template("4-index.html")
+    return render_template("5-index.html")
 
 
 if __name__ == "__main__":
