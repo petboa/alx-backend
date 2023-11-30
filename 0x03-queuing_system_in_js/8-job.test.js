783788cbcfd01ea94cf0dfd5da123eeb5148c165
petboa <petboa123@gmail.com>
2023-11-30 16:43:15 +0000

    redis

---
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -0,0 +1,49 @@
+#!/usr/bin/yarn test
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { createQueue } from 'kue';
+import createPushNotificationsJobs from './8-job.js';
+
+describe('createPushNotificationsJobs', () => {
+  const BIG_BROTHER = sinon.spy(console);
+  const QUEUE = createQueue({ name: 'push_notification_code_test' });
+
+  before(() => {
+    QUEUE.testMode.enter(true);
+  });
+
+  after(() => {
+    QUEUE.testMode.clear();
+    QUEUE.testMode.exit();
+  });
+
+  afterEach(() => {
+    BIG_BROTHER.log.resetHistory();
+  });
+
+  it('displays an error message if jobs is not an array', () => {
+    expect(
+      createPushNotificationsJobs.bind(createPushNotificationsJobs, {}, QUEUE)
+    ).to.throw('Jobs is not an array');
+  });
+
+  it('adds jobs to the queue with the correct type', (done) => {
+    expect(QUEUE.testMode.jobs.length).to.equal(0);
+    const jobInfos = [
+      {
+        phoneNumber: '44556677889',
+        message: 'Use the code 1982 to verify your account',
+      },
+      {
+        phoneNumber: '98877665544',
+        message: 'Use the code 1738 to verify your account',
+      },
+    ];
+    createPushNotificationsJobs(jobInfos, QUEUE);
+    expect(QUEUE.testMode.jobs.length).to.equal(2);
+    expect(QUEUE.testMode.jobs[0].data).to.deep.equal(jobInfos[0]);
+    expect(QUEUE.testMode.jobs[0].type).to.equal('push_notification_code_3');
+    QUEUE.process('push_notification_code_3', () => {
+      expect(
+        BIG_BROTHER.log
+          .calledWith('Notification job created:', QUEUE.testMode.jobs[0].id)
